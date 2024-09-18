36b18f76869976a63c289300b2bd260813964c10
PhysisOfWally <128063050+PhysisOfWally@users.noreply.github.com>
2024-09-18 18:34:40 -0300

    feat: Ajustes no dashboard

--- src/components/dashboard/Dashboard.tsx
+++ src/components/dashboard/Dashboard.tsx
@@ -7,6 +7,7 @@ import {
 import { AuthContext } from "@/contexts/AuthContext";
 import { buscar } from "@/services/Services";
 import { toastAlert } from "@/utils/toastAlert";
+import { format } from "date-fns";
 import { useContext, useEffect, useState } from "react";
 import { Bar, BarChart, XAxis } from "recharts";
 
@@ -20,6 +21,29 @@ const chartConfig = {
     color: "#FF0000",
   },
 } satisfies ChartConfig;
+
+interface VendaPorDia {
+  day: string;
+  PF: number;
+  PJ: number;
+}
+
+interface Venda {
+  id: number;
+  dataVenda: string;
+  nomeCliente: string;
+  cpf: string;
+  whatsapp: string;
+  telefone: string;
+  bandeiraCartao: string;
+  nomeBancoCartao: string;
+  valorVenda: number;
+  statusVenda: string;
+  observacoes: string;
+  vendedor: string;
+  tipoCliente: string; // "PF" ou "PJ"
+  cnpj?: string;
+}
 
 function Dashboard() {
     const [chartData, setChartData] = useState([]);
@@ -42,9 +66,34 @@ function Dashboard() {
         buscarVendas();
     }, []);
 
+    function agruparVendasPorDia(vendas: Venda[]): VendaPorDia[] {
+      // Reduzir as vendas para um objeto que agrupa por dia e conta PF e PJ
+      const vendasAgrupadas = vendas.reduce((acc, venda) => {
+        // Formatando a data para "dd/MM/yyyy"
+        const diaFormatado = format(new Date(venda.dataVenda), "dd/MM/yyyy");
+        
+        // Verifica se já existe um registro para essa data
+        if (!acc[diaFormatado]) {
+          acc[diaFormatado] = { day: diaFormatado, PF: 0, PJ: 0 };
+        }
+    
+        // Incrementa o contador de PF ou PJ com base no tipo de cliente
+        if (venda.tipoCliente === "PF") {
+          acc[diaFormatado].PF += 1;
+        } else if (venda.tipoCliente === "PJ") {
+          acc[diaFormatado].PJ += 1;
+        }
+    
+        return acc;
+      }, {} as Record<string, VendaPorDia>);
+    
+      // Converte o objeto agrupado em um array
+      return Object.values(vendasAgrupadas);
+    }
+
   return (
     <ChartContainer config={chartConfig} className="h-[200px] w-[95%]">
-      <BarChart accessibilityLayer data={chartData}>
+      <BarChart accessibilityLayer data={agruparVendasPorDia(chartData)}>
         <XAxis
           dataKey="day"
           tickLine={false}
